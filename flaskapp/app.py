012276e969dde9bace6cd9ec810e382828044383
Preston Smalley <prsmalley@gmail.com>
2026-05-08 10:25:41 -0500

    Add /version endpoint with tests

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -13,6 +13,10 @@ def greet():
     name = request.args.get("name", "world")
     return {"greeting": "Hi, " + name}
 
+@app.route("/version")
+def version():
+    return {"version": "1.0.0"}
+
 if __name__ == "__main__":
     host = os.environ.get("FLASK_HOST", "127.0.0.1")
     app.run(host=host, port=5000)
